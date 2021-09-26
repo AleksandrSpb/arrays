1b7f83bfd22d7cf129cd130ee2c7fa280f35ab58
AleksandrSpb <aleksandr_l@inbox.ru>
2021-09-27 01:56:55 +0300

    первая задача

---
+++ script.js
@@ -0,0 +1,7 @@
+const terrestrialPlanets = ['Меркурий', 'Венера', 'Земля', 'Марс'];
+const gasPlanets = ['Юпитер', 'Сатурн', 'Уран', 'Нептун'];
+const star = 'Солнце';
+
+const solarSystem = // напишите код здесь
+
+    console.log(solarSystem);
