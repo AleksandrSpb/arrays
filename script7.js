b6ecd0f4148d376ce751d2288e28d7689b797af8
AleksandrSpb <aleksandr_l@inbox.ru>
2021-09-28 00:06:05 +0300

    Функции обратного вызова и их аргументы

---
+++ script7.js
@@ -0,0 +1,14 @@
+/*Задача 7*/
+const partsOfWorld = [
+    'Европа',
+    'Азия',
+    'Африка',
+    'Австралия',
+    'Америка',
+    'Антарктида',
+    'Океания'
+];
+const theOldWorld = partsOfWorld.slice(0,3);
+console.log(theOldWorld);
+const theWholeWorld = partsOfWorld.slice();
+console.log(theWholeWorld);
---
+++ script8.js
@@ -0,0 +1,6 @@
+/*Задача 8*/
+const compass = ['N', 'E', 'S', 'W'];
+compass.splice(1,0,'NE');
+console.log(compass);
+compass.splice(1,1);
+console.log(compass);
