b0fca7247efa49b97f6132f081a2185d84f73ef2
AleksandrSpb <aleksandr_l@inbox.ru>
2021-09-28 01:41:28 +0300

    Вызовом метода map создайте новый массив из исходного.

---
+++ script12.js
@@ -0,0 +1,12 @@
+/*задача 12*/
+const arr = [
+    'Гравитационные силы',
+    'Электромагнитные силы',
+    'Ядерные силы',
+    'Слабые силы'
+];
+
+arr.forEach(function (element, index) {
+    console.log(`${index + 1}. ${element}`);
+
+});
---
+++ script13.js
@@ -0,0 +1,15 @@
+/*задача 13*/
+const characters = [
+    'Люк Скайуокер',
+    'Оби Ван',
+    'Чубакка',
+    'Энакин Скайуокер',
+    'Хан Соло',
+    'Палпатин'
+];
+
+const newCharacters = characters.map(function (character) {
+    // напишите свой код здесь
+});
+
+console.log(newCharacters);
